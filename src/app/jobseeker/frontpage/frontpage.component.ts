c523ad5a2f68b63f97b687ffc8e28d443c143f24
Aravind0145 <betharasi.aravind.c@revature.com>
2024-12-04 20:03:08 +0530

    Initial commit

---
+++ src/app/jobseeker/frontpage/frontpage.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+import { JobseekerserviceService } from '../../jobseekerservice.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-frontpage',
+  templateUrl: './frontpage.component.html',
+  styleUrl: './frontpage.component.css'
+})
+export class FrontpageComponent {
+  showLoginForm: boolean = false; // Controls login form visibility
+  email: string = '';
+  password: string = '';
+
+  toggleLoginForm(): void {
+    this.showLoginForm = !this.showLoginForm; // Toggle the form visibility
+  }
+
+  
+  constructor(
+    private jobService: JobseekerserviceService,
+    private router: Router
+  ) {}
+
+  login() {
+    if (this.email && this.password) {
+      // Perform login action (e.g., call authentication API)
+      console.log('Logging in with:', this.email, this.password);
+    } else {
+      console.log('Form is invalid');
+    }
+
+    console.log('User entered email:', this.email);
+    console.log('User entered password:', this.password);
+
+    this.jobService.jobseekerlogin(this.email, this.password).subscribe(
+      response => {
+        const role = response.role;
+        const fullName = response.fullName;
+        const id = response.id;
+        console.log('User role:', role);
+
+        if (role === 'jobseeker') {
+          console.log('Navigating to admin dashboard');
+          this.router.navigate(['/jobseeker/jobseekerhomepage'], {
+            queryParams: { fullName,id }
+          });        } else {
+          console.log('Invalid role');
+          alert('Access denied');
+        }
+      },
+      error => {
+        console.error('Login error:', error);
+        alert('Incorrect password or email please check it');
+        this.email = '';
+      this.password = '';
+      }
+    );
+  }
+
+
+
+
+}
